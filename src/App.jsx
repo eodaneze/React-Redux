6742c5fc986adb5f1effb84dd0d448b10f3f9ce3
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-03-17 07:04:41 -0700

    creating a counter app using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 const App = () => {
-  const counter = useSelector((state) => state.counter)
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch()
+  const increment = () => {
+      dispatch({type: "INCREMENT"})
+  }
+  const decrement = () => {
+    dispatch({type: "DECREMENT"})
+  }
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   )
 }
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,26 @@
 import {createStore} from 'redux'
 
-const reducerFn = (state = {counter: 20}, action) => {
-    return state
+const reducerFn = (state = {counter: 0}, action) => {
+    // redux limitation
+    // redux should be a synchronous function
+    // it should not mutate the orginal state
+
+    // if(action.type === "INCREMENT"){
+    //     return {counter: state.counter + 1}
+    // }else if(action.type === "DECREMENT"){
+    //     return {counter: state.counter - 1}
+    // }
+    // return state
+
+    switch(action.type){
+        case "INCREMENT":
+            return {counter: state.counter + 1}
+        case "DECREMENT":
+            return {counter: state.counter - 1}
+        default:
+            return state
+    }
+
 }
 
 const store = createStore(reducerFn)
